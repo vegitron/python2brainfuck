94ef4742b5286feabd8843cb061caa1ce7f63b0f
Patrick Michaud <pmichaud@uw.edu>
2016-09-23 12:59:35 -0700

    Adding a test for equality on statics

--- t/py2b/operators.py
+++ t/py2b/operators.py
@@ -5,7 +5,7 @@ import StringIO
 from util.run_bf import run
 
 class TestOperators(unittest.TestCase):
-    def test_equality(self):
+    def test_equality_true_false(self):
         emit_output = StringIO.StringIO()
         run_output = StringIO.StringIO()
         emitter = Emitter(stdout=emit_output)
@@ -23,6 +23,27 @@ class TestOperators(unittest.TestCase):
         memory_space = []
         run(emit_output.getvalue(), stdout=run_output, memory=memory_space)
         self.assertEquals(0, ord(run_output.getvalue()[0]))
+
+    def test_equality_statics(self):
+        emit_output = StringIO.StringIO()
+        run_output = StringIO.StringIO()
+        emitter = Emitter(stdout=emit_output)
+        python = """foo = "a" == "a"\nprint foo"""
+        builder = BFBuild(python, emit=emitter).emit_bf()
+        memory_space = []
+        run(emit_output.getvalue(), stdout=run_output, memory=memory_space)
+        self.assertEquals(1, ord(run_output.getvalue()[0]))
+
+        emit_output = StringIO.StringIO()
+        run_output = StringIO.StringIO()
+        emitter = Emitter(stdout=emit_output)
+        python = """foo = "a" == "b"\nprint foo"""
+        builder = BFBuild(python, emit=emitter).emit_bf()
+        memory_space = []
+        run(emit_output.getvalue(), stdout=run_output, memory=memory_space)
+        self.assertEquals(0, ord(run_output.getvalue()[0]))
+
+
 
     def test_equality_in_if(self):
         return
